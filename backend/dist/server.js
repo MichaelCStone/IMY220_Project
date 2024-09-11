9f5fec89febd9fd183c84c71a958131c6dde264c
Michael_C_Stone <mikestone2002@gmail.com>
2024-09-11 12:08:03 +0200

    fixed serving static pages

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,12 +1,13 @@
 "use strict";
 
 var _express = _interopRequireDefault(require("express"));
+var _path = _interopRequireDefault(require("path"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
 //CREATE APP
 var app = (0, _express["default"])();
 
 //SERVE A STATIC PAGE IN THE PUBLIC DIRECTORY
-app.use(_express["default"]["static"]("public"));
+app.use(_express["default"]["static"](_path["default"].join(__dirname, "../../frontend/public")));
 
 //PORT TO LISTEN TO
 app.listen(3000, function () {
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from "express";
+import path from "path";
 
  //CREATE APP
  const app = express();
 
  //SERVE A STATIC PAGE IN THE PUBLIC DIRECTORY
- app.use(express.static("public"));
+ app.use(express.static(path.join(__dirname, "../../frontend/public")));
 
  //PORT TO LISTEN TO
  app.listen(3000, () => {
