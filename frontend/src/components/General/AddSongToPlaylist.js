d3209f06208570ad7e8d6c9c8c958657099bb38c
Michael_C_Stone <mikestone2002@gmail.com>
2024-09-12 17:53:56 +0200

    Add a Song To a playlist component

--- frontend/src/components/General/AddSongToPlaylist.js
+++ frontend/src/components/General/AddSongToPlaylist.js
@@ -6,50 +6,78 @@ class AddSongToPlaylist extends Component
     constructor(props) 
     {
         super(props);
+
         this.state = {
-            
-            songTitle: '',
-            artist: '',
-            playlist: ''
+            selectedSong: ''
         };
     }
 
-    handleChange = (event) => {
-        this.setState({ [event.target.name]: event.target.value });
-    };
+    handleSongChange = (event) => {
+        this.setState({ selectedSong: event.target.value });
+    }
 
     handleSubmit = (event) => {
         event.preventDefault();
-        // Placeholder for adding functionality (e.g., call a function to add song to playlist)
-        console.log('Adding song to playlist:', this.state);
-    };
+        const { selectedSong } = this.state;
+        const { onAddSongToPlaylist } = this.props;
+
+        if (selectedSong && onAddSongToPlaylist) {
+            onAddSongToPlaylist(selectedSong);
+            this.setState({ selectedSong: '' });
+        }
+    }
 
     render() 
     {
+        const { songs } = this.props;
+        const { selectedSong } = this.state;
+
         return (
-            <div className="add-song-to-playlist">
-                <h3>Add Song to Playlist</h3>
+            <div>
+                <h2>Add a Song to Playlist</h2>
                 <form onSubmit={this.handleSubmit}>
                     <div>
-                        <label>Song Title:</label>
-                        <input type="text" name="songTitle" value={this.state.songTitle} onChange={this.handleChange} required />
+                        <label>Select Song:</label>
+                        
+                        <select value={selectedSong} onChange={this.handleSongChange} required>
+                            <option value="">Select a song</option>
+                            {songs.map((song, index) => (
+                                <option key={index} value={song.title}>{song.title}</option>
+                            ))}
+                        </select>
                     </div>
-
-                    <div>
-                        <label>Artist:</label>
-                        <input type="text" name="artist" value={this.state.artist} onChange={this.handleChange} required />
-                    </div>
-
-                    <div>
-                        <label>Playlist:</label>
-                        <input type="text" name="playlist" value={this.state.playlist} onChange={this.handleChange} required />
-                    </div>
-
                     <button type="submit">Add Song</button>
                 </form>
             </div>
         );
     }
+
+    // render() 
+    // {
+    //     return (
+    //         <div className="add-song-to-playlist">
+    //             <h3>Add Song to Playlist</h3>
+    //             <form onSubmit={this.handleSubmit}>
+    //                 <div>
+    //                     <label>Song Title:</label>
+    //                     <input type="text" name="songTitle" value={this.state.songTitle} onChange={this.handleChange} required />
+    //                 </div>
+
+    //                 <div>
+    //                     <label>Artist:</label>
+    //                     <input type="text" name="artist" value={this.state.artist} onChange={this.handleChange} required />
+    //                 </div>
+
+    //                 <div>
+    //                     <label>Playlist:</label>
+    //                     <input type="text" name="playlist" value={this.state.playlist} onChange={this.handleChange} required />
+    //                 </div>
+
+    //                 <button type="submit">Add Song</button>
+    //             </form>
+    //         </div>
+    //     );
+    // }
 }
 
 export default AddSongToPlaylist;
