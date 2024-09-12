2ef46ffc54e7449e290907e5418237e703df1808
Michael_C_Stone <mikestone2002@gmail.com>
2024-09-12 18:52:15 +0200

    update profile and playlist page

--- frontend/src/pages/PlaylistPage.js
+++ frontend/src/pages/PlaylistPage.js
@@ -1,15 +1,20 @@
 // u21497682 - Michael Stone
-import React from 'react';
+import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import Navigation from "../components/General/Navigation"
 
-const PlaylistPage = () => {
-  return (
-    <div>
-      <h1>Playlist Page!</h1>
-      <Navigation />
-    </div>
-  );
+class PlaylistPage extends Component
+{
+  render()
+  {
+    return (
+      <div>
+        <h1>Playlist Page!</h1>
+        <Navigation />
+      </div>
+    );
+  }
 };
+
 
 export default PlaylistPage;
--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,15 +1,19 @@
 // u21497682 - Michael Stone
-import React from 'react';
+import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import Navigation from "../components/General/Navigation"
 
-const ProfilePage = () => {
-  return (
-    <div>
-      <h1>Profile Page!</h1>
-      <Navigation />
-    </div>
-  );
+class ProfilePage extends Component
+{
+  render()
+  {
+    return (
+      <div>
+        <h1>Profile Page!</h1>
+        <Navigation />
+      </div>
+    );
+  }
 };
 
 export default ProfilePage;
