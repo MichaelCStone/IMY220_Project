328d0e55b436786ad65d90588f15bd54e47c0e00
Michael_C_Stone <mikestone2002@gmail.com>
2024-09-12 16:48:42 +0200

    Profile Preview component

---
+++ frontend/src/components/General/ProfilePreview.js
@@ -0,0 +1,17 @@
+// u21497682 - Michael Stone
+import React, { Component } from 'react';
+
+class ProfilePreview extends Component 
+{
+  render() 
+  {
+    return (
+      <div>
+        {/* Implement some kind of image */}
+        <h3>{this.props.profile.name}</h3>
+      </div>
+    );
+  }
+}
+
+export default ProfilePreview;
